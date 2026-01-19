cc33b80b35285d5fa197d6eb90e9af60a0f6e65b
Martin Lissmyr <martin@lissmyr.com>
2026-01-19 22:10:22 +0100

    Fixes warnings

--- electron/handlers/devMode.ts
+++ electron/handlers/devMode.ts
@@ -1,7 +1,6 @@
 // electron/handlers/devMode.ts
 import { safeHandle } from '../utils/ipcHandler';
 import { VITE_DEV_SERVER_URL } from '../main';
-import { BrowserWindow } from 'electron';
 
 /**
  * Registers IPC handlers for dev mode detection
